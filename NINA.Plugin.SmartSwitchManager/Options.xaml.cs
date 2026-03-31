7c49d7d46658a45e45116ad2801cb0f45abcfdc4
DiMoGro1 <110323263+DiMoGro1@users.noreply.github.com>
2026-03-31 19:51:41 +0200

    ci: sync images to wiki and fix paths

--- NINA.Plugin.SmartSwitchManager/Options.xaml.cs
+++ NINA.Plugin.SmartSwitchManager/Options.xaml.cs
@@ -9,5 +9,17 @@ namespace NINA.Plugin.SmartSwitchManager {
         public Options() {
             InitializeComponent();
         }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e) {
+            try {
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo {
+                    FileName = e.Uri.AbsoluteUri,
+                    UseShellExecute = true
+                });
+                e.Handled = true;
+            } catch {
+                // Silently fail if browser can't be opened
+            }
+        }
     }
 }
